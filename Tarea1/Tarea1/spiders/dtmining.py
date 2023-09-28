a0df5c9eae228ec2af4efc935123b2ca08ffea02
Brayton Chana Goméz <brayton-leonardo-12@hotmail.com>
2023-09-28 18:12:15 -0500

    He actualizado los comandos de recoleccion de la araña ahora con scrapy crawl dtmining -O prueba.json he guardado los datos que he extraido
    Datos extraidos a prueba.json

--- Tarea1/Tarea1/spiders/dtmining.py
+++ Tarea1/Tarea1/spiders/dtmining.py
@@ -4,7 +4,18 @@ import scrapy
 class DtminingSpider(scrapy.Spider):
     name = "dtmining"
     allowed_domains = ["books.toscrape.com"]
-    start_urls = ["https://books.toscrape.com/index.html"]
+    start_urls = ["https://books.toscrape.com/catalogue/page-1.html"]
 
     def parse(self, response):
-        pass
+        products = response.css('article.product_pod')
+        for product in products:
+            yield {
+                'nombre' : product.css('h3').css('a::attr(title)').get(),
+                'precio' : product.css('article.product_pod').css('div.product_price').css('p.price_color::text').get(),
+                'url' : product.css('h3').css('a::attr(href)').get()
+            }
+        siguiente_pag = response.css('li.next').css('a::attr(href)').get()
+
+        if siguiente_pag is not None:
+            siguiente_pag_url = "https://books.toscrape.com/catalogue/"+siguiente_pag
+            yield response.follow(siguiente_pag_url,callback=self.parse)
